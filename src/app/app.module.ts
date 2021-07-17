fdd4f9a9f07c9ff0bd628c5ebfe095570a735fad
Robert Maina <robert.maina@student.moringaschool.com>
2021-07-17 13:36:26 +0300

    added quote-form component and added a form o the component's html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { FormComponent } from './form/form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    FormComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
